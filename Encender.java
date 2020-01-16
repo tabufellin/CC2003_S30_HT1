2f2ec2358ec4a4b3af3d789788d2c54037d34a83
PingMaster99 <59905458+PingMaster99@users.noreply.github.com>
2020-01-16 11:44:05 -0600

    Primeros métodos e interface

---
+++ Encender.java
@@ -0,0 +1,57 @@
+public class MaquinaRadio implements Radio {
+
+    private boolean encendido;
+    private String estacionGuardada;
+    private int currentAmStation = 550;
+    private double currentFmStation = 90.1;
+    private boolean tipoFrecuencia = false;   // false para FM, true para AM
+
+    public void encender() {
+        this.encendido = true;
+    }
+
+    public void apagar() {
+        this.encendido = false;
+    }
+
+    public void cambioTipoFrecuencia(boolean tipo) {
+        if(tipo) {
+            this.tipoFrecuencia = !this.tipoFrecuencia;
+        }
+    }
+
+    public void cambioEstacion(boolean tipo, boolean tipoFrecuencia) {
+        // AM frequency 
+        if(tipoFrecuencia) {
+            if(tipo && this.currentAmStation < 1610) {
+                this.currentAmStation += 10;
+            } else {
+                this.currentAmStation -= 10;
+            }
+
+            // Checks if the station is within the defined range, and overflows or underflows accordingly
+            if(this.currentAmStation > 1610) {
+                this.currentAmStation = 530;
+            } else if(this.currentAmStation < 530) {
+                this.currentAmStation = 1610;
+            }
+
+        // FM frequency
+        } else {
+            if(tipo && this.currentFmStation < 107.9) {
+                this.currentFmStation += 0.2;
+            } else {
+                this.currentFmStation -= 0.2;
+            }
+
+            // Checks if the station is within the defined range, and overflows or underflows accordingly
+            if(this.currentFmStation > 107.9) {
+                this.currentFmStation = 87.9;
+            } else if(this.currentFmStation < 87.9) {
+                this.currentFmStation = 107.9;
+            }
+        }
+    }
+
+
+}
