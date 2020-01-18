b8160ff7365d677fdd0b2188507bf09a0005d3b2
PingMaster99 <59905458+PingMaster99@users.noreply.github.com>
2020-01-17 19:29:15 -0600

    Actualización GUI

--- RadioGUI.java
+++ RadioGUI.java
@@ -10,12 +10,17 @@ public class RadioGUI {
     private JTextField radioMatrixTextField;
     private JButton button1;
     private JButton button2;
-    private JButton button7;
-    private JButton button8;
-    private JButton button9;
-    private JButton button10;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JButton button6;
+    private JButton a12Button;
+    private JButton a11Button;
+    private JButton a8Button;
+    private JButton a7Button;
+    private JButton a5Button;
+    private JButton a9Button;
+    private JButton a6Button;
+    private JButton a10Button;
+    private JButton a1Button;
+    private JButton a2Button;
+    private JButton a3Button;
+    private JButton a4Button;
+    private JRadioButton guardarEstaciónRadioButton;
 }
