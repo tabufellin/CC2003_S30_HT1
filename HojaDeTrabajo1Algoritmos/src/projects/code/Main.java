f2b04bf6b6f2812c3eed62a436e1d3eec9f48dee
PingMaster99 <59905458+PingMaster99@users.noreply.github.com>
2020-01-18 16:24:09 -0600

    Folder del proyecto IntelliJ

---
+++ HojaDeTrabajo1Algoritmos/src/projects/code/Main.java
@@ -0,0 +1,18 @@
+package projects.code;
+
+import com.RadioGUI;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        // Inicia la ventana del radio
+        RadioGUI graficosRadio = new RadioGUI();
+        graficosRadio.setVisible(true);
+
+    }
+
+}
