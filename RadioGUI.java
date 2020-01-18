7fec2cb8a9b2ef706fa0541d95ebca69028bdd14
PingMaster99 <59905458+PingMaster99@users.noreply.github.com>
2020-01-17 19:08:04 -0600

    Interfaz (prototipo)
    Todavía no estoy seguro cómo hacer el link con el código, pero así va el GUI.

---
+++ RadioGUI.java
@@ -0,0 +1,21 @@
+package com;
+
+import javax.swing.*;
+
+public class RadioGUI {
+    private JPanel panel1;
+    private JButton offButton;
+    private JButton onButton;
+    private JRadioButton AMFMRadioButton;
+    private JTextField radioMatrixTextField;
+    private JButton button1;
+    private JButton button2;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+}
