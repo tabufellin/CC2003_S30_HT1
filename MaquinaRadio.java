52cdfdbd60ba94c67d1d6cd86b69f5a3d6703f02
Diego Sebastián González Solís <49197018+tabufellin@users.noreply.github.com>
2020-01-19 22:03:31 -0600

    Javadoc MaquinaRadio

--- MaquinaRadio.java
+++ MaquinaRadio.java
@@ -1,3 +1,16 @@
+/**
+* <h1>Main</h1>
+* This is the main class to run the Radio application, it displays
+* the radio window which simulates the behavior of a traditional 
+* radio.
+* <p>
+*
+*
+* @author Sebastian Gonzales (tabufellin) Pablo Ruiz (PingMaster99)
+* @version 1.0
+* @since 2020-01-19
+*/
+
 public class MaquinaRadio implements Radio {
 
     static final String UNDER_DEVELOPMENT_METHOD = "Method is under development";
@@ -15,6 +28,13 @@ public class MaquinaRadio implements Radio {
     // hay que darle la estacion que va a estar en ese momento
     double currentStation;
 
+   /**
+   * This method is used to set the radio as on;
+   * it also creates arrays used for the buttons
+   * needed to save stations.
+   * @param Unused
+   * @return Nothing
+   */
     public MaquinaRadio() {
         int cuantasEstaciones = 12;
 
@@ -30,16 +50,30 @@ public class MaquinaRadio implements Radio {
 
     }
 
-
+   /**
+   * This method is used to set the radio as on.
+   * @param Unused
+   * @return Nothing
+   */
     public void encender() {
         this.encendido = true;
     }
 
+   /**
+   * This method is used to set the radio as off.
+   * @param Unused
+   * @return Nothing
+   */
     public void apagar() {
         this.encendido = false;
     }
 
-    // Creo que ya esta terminado este metodo
+   /**
+   * This method is used to change the frequency used in 
+   * the radio (AM or FM). 
+   * @param tipo changes the frequency to AM or FM
+   * @return Nothing
+   */
     public void cambioTipoFrecuencia(boolean tipo) {
         this.tipoFrecuencia = !this.tipoFrecuencia;
         if (tipo) {
@@ -50,6 +84,12 @@ public class MaquinaRadio implements Radio {
 
     }
 
+   /**
+   * This method is used to change the current station in the radio.
+   * @param tipo boolean indicates if the station will increase or decrease
+   * @param tipoFrecuencia indicates AM (true) or FM (false) frequency
+   * @return Nothing
+   */
     public void cambioEstacion(boolean tipo, boolean tipoFrecuencia) {
         // AM frequency
         if (tipoFrecuencia) {
@@ -89,8 +129,12 @@ public class MaquinaRadio implements Radio {
         }
     }
 
-//***********************INCOMPLETE METHODS*****************************************************************************
-
+   /**
+   * This method is used to change the current station in the radio.
+   * @param tipo boolean indicates if the station will increase or decrease
+   * @param tipoFrecuencia indicates AM (true) or FM (false) frequency
+   * @return Nothing
+   */
     public void guardarEstacion(double estacion, boolean tipoFrecuencia, int boton){
 
         boton -=1;
@@ -102,6 +146,12 @@ public class MaquinaRadio implements Radio {
         }
     }
 
+   /**
+   * This method is used to select a saved station in a button. 
+   * @param tipoFrecuencia indicates AM (true) or FM (false) frequency
+   * @param boton one of the 12 buttons present in the radio
+   * @return double with the station that is played
+   */
     public double seleccionarEstacion(boolean tipoFrecuencia, int boton) {
 
         boton -= 1;
@@ -124,27 +174,57 @@ public class MaquinaRadio implements Radio {
 
 
     // Methods to obtain current value of variables; getters
-
+    
+   /**
+   * This method gets the current FM station 
+   * @param Unused
+   * @return double with current FM station 
+   */
     public double getCurrentFmStation() {
         return this.currentFmStation;
     }
 
+   /**
+   * This method gets the on/off state 
+   * @param Unused
+   * @return boolean that indicates if the radio is on (true) or off (false)
+   */
     public boolean getEncendido() {
         return encendido;
     }
 
+   /**
+   * This method gets the saved station  
+   * @param Unused
+   * @return String with the saved station
+   */
     public String getEstacionGuardada() {
         return estacionGuardada;
     }
 
+   /**
+   * This method gets the current AM station 
+   * @param Unused
+   * @return int with the current AM station 
+   */
     public int getCurrentAmStation() {
         return currentAmStation;
     }
 
+   /**
+   * This method gets the frequency type
+   * @param Unused
+   * @return boolean that indicates AM (true) or FM (false) frequency
+   */
     public boolean getTipoFrecuencia() {
         return tipoFrecuencia;
     }
 
+   /**
+   * This method sets the on / off value of the radio
+   * @param boolean valor that indicates on (true) or off (false)
+   * @return Nothing
+   */
     public void setEncendido (boolean valor){
         encendido = valor;
     }
