3492ca42aadaeb269e4d173eb782a4130ef37e58
Diego Sebastián González Solís <49197018+tabufellin@users.noreply.github.com>
2020-01-19 21:21:09 -0600

    Javadoc comments

--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
+/**
+* <h1>Main</h1>
+* This is the main class to run the Radio application, it displays
+* the radio window which simulates the behavior of a traditional 
+* radio.
+* <p>
+*
+*
+* @author Sebastian Gonzales (tabufellin) Pablo Ruiz (PingMaster99)
+* @version 1.0
+* @since 2020-01-19
+*/
+
 public class Main {
 
     public static void main(String[] args) {
 
 
-        // Inicia la ventana del radio
+        // Initializes the radio window.
         RadioGUI graficosRadio = new RadioGUI();
         graficosRadio.setVisible(true);
 
