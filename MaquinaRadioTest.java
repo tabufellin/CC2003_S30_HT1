fef5d50a818018f9d1ae2e8713fde2f94daadf82
PingMaster99 <59905458+PingMaster99@users.noreply.github.com>
2020-01-25 19:44:47 -0600

    Junit Tests Final

--- MaquinaRadioTest.java
+++ MaquinaRadioTest.java
@@ -1,52 +1,52 @@
-//import static org.junit.Assert.*;
-//
-//public class MaquinaRadioTest {
-//
-////    @org.junit.Test
-////    public void encender() {
-////        MaquinaRadio instance = new MaquinaRadio();
-////        instance.encender();
-////        assertEquals(true, instance.getEncendido());
-////    }
-////
-////    // Test que va a fallar
-////    @org.junit.Test
-////    public void apagar() {
-////        MaquinaRadio instance = new MaquinaRadio();
-////        instance.apagar();
-////        assertEquals(true, instance.getEncendido());
-////    }
-//
-//    @org.junit.Test
-//    public void cambioTipoFrecuencia() {
-//        MaquinaRadio instance = new MaquinaRadio();
-//        instance.cambioTipoFrecuencia(true);
-//        assertEquals(true, true);
-//    }
-//
-//    @org.junit.Test
-//    public void cambioEstacion() {
-//        MaquinaRadio instance = new MaquinaRadio();
-//        instance.cambioEstacion(true, true);
-//        assertEquals(560, instance.getCurrentAmStation());
-//
-//        instance.cambioEstacion(false, true);
-//        assertEquals(550, instance.getCurrentAmStation());
-//
-//        instance.cambioEstacion(false, true);
-//        instance.cambioEstacion(false, true);
-//        instance.cambioEstacion(false, true);
-//        assertEquals(1600, instance.getCurrentAmStation());
-//
-//        instance.cambioEstacion(true, true);
-//        assertEquals(90.1, instance.getCurrentFmStation(), 0);
-//
-//        instance.cambioEstacion(true, false);
-//        assertEquals(90.3, instance.getCurrentFmStation(), 0);
-//
-//        instance.cambioEstacion(false, false);
-//        assertEquals(90.1, instance.getCurrentFmStation(), 0);
-//
-//    }
-//
-//}
+import static org.junit.Assert.*;
+
+public class MaquinaRadioTest {
+
+    @org.junit.Test
+    public void encenderApagar() {
+        MaquinaRadio instance = new MaquinaRadio();
+        instance.encenderApagar();
+        assertEquals(true, instance.getEstado());
+    }
+
+//    // Test que va a fallar
+/*    @org.junit.Test
+    public void apagar() {
+        MaquinaRadio instance = new MaquinaRadio();
+        instance.apagar();
+        assertEquals(true, instance.getEncendido());
+    }*/
+
+    @org.junit.Test
+    public void cambioTipoFrecuencia() {
+        MaquinaRadio instance = new MaquinaRadio();
+        instance.cambioTipoFrecuencia(true);
+        assertEquals(true, true);
+    }
+
+    @org.junit.Test
+    public void cambioEstacion() {
+        MaquinaRadio instance = new MaquinaRadio();
+        instance.cambioEstacion(true, true);
+        assertEquals(560, instance.getEstacion(), 0);
+
+        instance.cambioEstacion(false, true);
+        assertEquals(550, instance.getEstacion(), 0);
+
+        instance.cambioEstacion(false, true);
+        instance.cambioEstacion(false, true);
+        instance.cambioEstacion(false, true);
+        assertEquals(1610, instance.getEstacion(), 0);
+
+        instance.cambioEstacion(true, false);
+        assertEquals(90.3, instance.getEstacion(), 0);
+
+        instance.cambioEstacion(true, false);
+        assertEquals(90.5, instance.getEstacion(), 0);
+
+        instance.cambioEstacion(false, false);
+        assertEquals(90.3, instance.getEstacion(), 0);
+
+    }
+
+}
